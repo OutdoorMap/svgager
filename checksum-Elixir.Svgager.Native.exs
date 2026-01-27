0efd059cc5f360eb95459b3eaa2ad84b72449b95
Joakim Nylen <git@joakim.nylen.nu>
2026-01-27 10:47:29 +0100

    fix: bump

--- checksum-Elixir.Svgager.Native.exs
+++ checksum-Elixir.Svgager.Native.exs
@@ -1,10 +1,10 @@
 %{
-  "libsvgager_native-v0.1.0-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:bc3662ec14942988ccaab2c2fa2ee63154cbd27ee1fa906c0f04efce5c0a9bbc",
-  "libsvgager_native-v0.1.0-nif-2.15-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:f4957f599421a27b4c3f2a228bd8969eedf56464367f7944698dc13571170d28",
-  "libsvgager_native-v0.1.0-nif-2.15-arm-unknown-linux-gnueabihf.so.tar.gz" => "sha256:28b6cddfda589d33b75526c07f07b0ea18b5fb21e8890c34ba6af0e8beb43cd4",
-  "libsvgager_native-v0.1.0-nif-2.15-riscv64gc-unknown-linux-gnu.so.tar.gz" => "sha256:4489abcb0de646548a0d16019a77cdcffd889017b7ae10b21ffdd7bf5e4ac64a",
-  "libsvgager_native-v0.1.0-nif-2.15-x86_64-apple-darwin.so.tar.gz" => "sha256:2d431d30fa15232a02415267b921c167ca1a6043ccd4b3afb5e4fbd7da9314ab",
-  "libsvgager_native-v0.1.0-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:59a4069ecb6a3b8294d1c8d707adebe7c96eef6fb8db000fe40f4fbc2d0e802a",
-  "svgager_native-v0.1.0-nif-2.15-x86_64-pc-windows-gnu.dll.tar.gz" => "sha256:7fe66e195a49735b6e5e8c3304bb6957d4f8657e178320d0914e55ab858786bb",
-  "svgager_native-v0.1.0-nif-2.15-x86_64-pc-windows-msvc.dll.tar.gz" => "sha256:74966aa5ccd1c71c859e24e18a71ec673f9e467e35a06701698df540ef4e108f",
+  "libsvgager_native-v0.2.0-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:a986719a9f429a785083531e149d394f7fd4f8ca5ebf2049e9a93aed8127ae50",
+  "libsvgager_native-v0.2.0-nif-2.15-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:befe20f01c888262b44ee8b9e3046cff5498c85c5981adde78599b5a9176f410",
+  "libsvgager_native-v0.2.0-nif-2.15-arm-unknown-linux-gnueabihf.so.tar.gz" => "sha256:de822cf78de205f51d7ac43ca1425777d1e58499e4d1c1ba5fe7393dd3e29bc4",
+  "libsvgager_native-v0.2.0-nif-2.15-riscv64gc-unknown-linux-gnu.so.tar.gz" => "sha256:2ecd0197624f49decc09ba79557e7cd7e66bae485eecd6f457dc3175fc101301",
+  "libsvgager_native-v0.2.0-nif-2.15-x86_64-apple-darwin.so.tar.gz" => "sha256:ff47ab3fa48b857ce6a3738cdc5b092e4a5cf0cd8c43ef618ecb795507638ed6",
+  "libsvgager_native-v0.2.0-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:602e6a4e0ce6fa0cc4fb21da229c600d7bb7df6ef830bcd0c4baab5f57cafacf",
+  "svgager_native-v0.2.0-nif-2.15-x86_64-pc-windows-gnu.dll.tar.gz" => "sha256:9e4b92ece7bc702e351324de28796c7c0b353bf31b50ab04084c198e04e9bf04",
+  "svgager_native-v0.2.0-nif-2.15-x86_64-pc-windows-msvc.dll.tar.gz" => "sha256:84fd0f9b6da17c7004eda150d57d83dcda993f6a707a4f2c7047b05db6ac431d",
 }
