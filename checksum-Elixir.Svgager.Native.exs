c444bdff4d76d1088966630e40c71c7a5716732a
Joakim Nylen <git@joakim.nylen.nu>
2026-01-27 10:01:18 +0100

    fix: bump checksum

--- checksum-Elixir.Svgager.Native.exs
+++ checksum-Elixir.Svgager.Native.exs
@@ -1,10 +1,10 @@
 %{
-  # This file contains checksums for precompiled NIFs.
-  # It will be automatically generated when you build and release precompiled binaries.
-  #
-  # To generate this file after creating GitHub releases with precompiled .tar.gz files:
-  # 1. Update the base_url in lib/svgager/native.ex with your GitHub repository
-  # 2. Run: mix rustler_precompiled.download Svgager.Native --all --print-checksum > checksum-Elixir.Svgager.Native.exs
-  #
-  # For development with local builds, this file can remain empty.
+  "libsvgager_native-v0.1.0-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:bc3662ec14942988ccaab2c2fa2ee63154cbd27ee1fa906c0f04efce5c0a9bbc",
+  "libsvgager_native-v0.1.0-nif-2.15-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:f4957f599421a27b4c3f2a228bd8969eedf56464367f7944698dc13571170d28",
+  "libsvgager_native-v0.1.0-nif-2.15-arm-unknown-linux-gnueabihf.so.tar.gz" => "sha256:28b6cddfda589d33b75526c07f07b0ea18b5fb21e8890c34ba6af0e8beb43cd4",
+  "libsvgager_native-v0.1.0-nif-2.15-riscv64gc-unknown-linux-gnu.so.tar.gz" => "sha256:4489abcb0de646548a0d16019a77cdcffd889017b7ae10b21ffdd7bf5e4ac64a",
+  "libsvgager_native-v0.1.0-nif-2.15-x86_64-apple-darwin.so.tar.gz" => "sha256:2d431d30fa15232a02415267b921c167ca1a6043ccd4b3afb5e4fbd7da9314ab",
+  "libsvgager_native-v0.1.0-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:59a4069ecb6a3b8294d1c8d707adebe7c96eef6fb8db000fe40f4fbc2d0e802a",
+  "svgager_native-v0.1.0-nif-2.15-x86_64-pc-windows-gnu.dll.tar.gz" => "sha256:7fe66e195a49735b6e5e8c3304bb6957d4f8657e178320d0914e55ab858786bb",
+  "svgager_native-v0.1.0-nif-2.15-x86_64-pc-windows-msvc.dll.tar.gz" => "sha256:74966aa5ccd1c71c859e24e18a71ec673f9e467e35a06701698df540ef4e108f",
 }
