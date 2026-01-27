e0e0c190e8e505093436b7f11db4730fec937f68
Joakim Nylen <git@joakim.nylen.nu>
2026-01-27 09:56:07 +0100

    fix

--- mix.exs
+++ mix.exs
@@ -1,10 +1,12 @@
 defmodule Svgager.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+
   def project do
     [
       app: :svgager,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.19",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
